cd569d818d701c53e57b3c90f61b8d9bbc16f6d7
Alexander Robbins <alexi@tamber.com>
2017-06-01 21:21:24 -0400

    recurring charge Get and Activate

--- recurring_charges.go
+++ recurring_charges.go
@@ -10,17 +10,19 @@ const billingBasePath = "admin/recurring_application_charges"
 
 // Theme represents a Shopify theme
 type RecurringApplicationCharge struct {
-	CappedAmount    decimal.Decimal `json:"capped_amount"`
-	ConfirmationUrl string          `json:"confirmation_url"`
-	ReturnUrl       string          `json:"return_url"`
-	Name            string          `json:"name"`
-	Price           decimal.Decimal `json:"price"`
-	CreatedAt       *time.Time      `json:"created_at"`
-	UpdatedAt       *time.Time      `json:"updated_at"`
-	Id              int             `json:"id"`
-	Status          string          `json:"status"`
-	Terms           string          `json:"terms"`
-	Test            string          `json:"test,omitempty"`
+	CappedAmount       decimal.Decimal `json:"capped_amount"`
+	ConfirmationUrl    string          `json:"confirmation_url"`
+	ReturnUrl          string          `json:"return_url"`
+	DecoratedReturnUrl string          `json:"decorated_return_url"`
+	Name               string          `json:"name"`
+	Price              decimal.Decimal `json:"price"`
+	CreatedAt          *time.Time      `json:"created_at"`
+	UpdatedAt          *time.Time      `json:"updated_at"`
+	Id                 int             `json:"id"`
+	Status             string          `json:"status"`
+	Terms              string          `json:"terms"`
+	Test               string          `json:"test,omitempty"`
+	Accepted           bool            `json:"accepted,omitempty"`
 }
 
 // Represents the result from the themes/X.json endpoint
@@ -34,9 +36,24 @@ type RecurringApplicationChargeService struct {
 	client *Client
 }
 
-// Create a new webhook
 func (s *RecurringApplicationChargeService) Create(charge RecurringApplicationCharge) (*RecurringApplicationCharge, error) {
 	path := fmt.Sprintf("%s.json", billingBasePath)
+	wrappedData := RecurringApplicationChargeResource{RecurringApplicationCharge: &charge}
+	resource := new(RecurringApplicationChargeResource)
+	err := s.client.Post(path, wrappedData, resource)
+	return resource.RecurringApplicationCharge, err
+}
+
+// Get individual order
+func (s *RecurringApplicationChargeService) Get(chargeID int, options interface{}) (*RecurringApplicationCharge, error) {
+	path := fmt.Sprintf("%s/%d.json", billingBasePath, chargeID)
+	resource := new(RecurringApplicationChargeResource)
+	err := s.client.Get(path, resource, options)
+	return resource.RecurringApplicationCharge, err
+}
+
+func (s *RecurringApplicationChargeService) Activate(charge RecurringApplicationCharge) (*RecurringApplicationCharge, error) {
+	path := fmt.Sprintf("%s/%d/activate.json", billingBasePath, charge.Id)
 	wrappedData := RecurringApplicationChargeResource{RecurringApplicationCharge: &charge}
 	resource := new(RecurringApplicationChargeResource)
 	err := s.client.Post(path, wrappedData, resource)
